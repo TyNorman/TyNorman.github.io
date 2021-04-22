4ccf1e4dd752817de7d77acad0987a2ac7270a5f
Tyler Norman <TylerNorman881@hotmail.com>
2021-04-22 18:19:48 -0400

    Profile Icon Test, Text Updates, Initial Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import profilePic from './assets/aboutme.jpg';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" fill="#ffffff"/>
+        {/* <img src={logo} className="App-logo" alt="logo" fill="#ffffff"/> */}
+
+        <div class="profile-icon">
+                <img src={profilePic} />
+        </div>
+
         <h1>Hello World!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload. Or don't whatever
+          Bing bong I'm slowly making a website here
         </p>
         <a
           className="App-link"
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="Header">
+    <nav className="Nav">
+    <a href="/">Home</a>
+    <a href="/">About Me</a>
+    <a href="/">Contact</a>
+    </nav>
+    </header>
+  );
+}
